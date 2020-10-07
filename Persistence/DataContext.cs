b3e58b1840c7ff0f40a2042ba1810885f8f7a4f8
Jack Grant <51317475+JGrant97@users.noreply.github.com>
2020-10-07 23:46:15 +0100

    initial commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value { id = 1, name = "Values 101" },
+                    new Value { id = 2, name = "Values 102" },
+                    new Value { id = 3, name = "Values 103" }
+                );
+        }
+    }
+}
