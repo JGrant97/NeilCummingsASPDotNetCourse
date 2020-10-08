e65ab88da63707d51bd57f7f1faa3510939f5b89
Jack Grant <51317475+JGrant97@users.noreply.github.com>
2020-10-08 22:29:02 +0100

    Start of section 4

---
+++ Persistence/Migrations/20201008203440_ActivityEntityAdded.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class ActivityEntityAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.CreateTable(
+                name: "activities",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(nullable: false),
+                    title = table.Column<string>(nullable: true),
+                    description = table.Column<string>(nullable: true),
+                    category = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    city = table.Column<string>(nullable: true),
+                    venue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_activities", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "activities");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_values",
+                table: "values");
+
+            migrationBuilder.RenameTable(
+                name: "values",
+                newName: "Values");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Values",
+                table: "Values",
+                column: "id");
+        }
+    }
+}
